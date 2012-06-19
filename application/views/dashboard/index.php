0ad8d970acaec7517c52caa10738b3e9af9f194d
peterpan <isaak@mogetutu.com>
2012-06-19 17:30:34 +0300

    Added stats bar on dashboard

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,3 +1,19 @@
 <div class="hero-unit">
 	<h1>Welcome to Mfarm</h1>
 </div>
+<div class="well summary">
+	<ul>
+		<li>
+			<a href="#"><span class="count"><?=$posts ?></span> Posts</a>
+		</li>
+		<li>
+			<a href="#"><span class="count">27</span> Orders</a>
+		</li>
+		<li>
+			<a href="#"><span class="count">7</span> Invites</a>
+		</li>
+		<li class="last">
+			<a href="#"><span class="count">5</span> Crops</a>
+		</li>
+	</ul>
+</div>
