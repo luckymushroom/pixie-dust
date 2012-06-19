f3e7ea820f0a02f312f685f54422f668e6dc2981
peterpan <isaak@mogetutu.com>
2012-06-19 17:30:19 +0300

    Added header on account creation page

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -5,6 +5,7 @@
 	<input type="text" name="email" value="<?=set_value('email');?>" placeholder="Email" rel="popover" data-content="Enter your email address." data-original-title="Email" required="required" class="input-xlarge">
     <input type="password" name="password" value="" placeholder="Password" class="input-xlarge"  rel="popover" data-content="Quickly,Nobody is looking enter password." data-original-title="Password" required>
     <label class="checkbox"><?php echo form_checkbox('remember', '1', FALSE);?>Remember Me</label>
+    <a href="<?=site_url('auth/create_user');?>" class="btn btn-warning" title="Create Account">Create Account</a>
     <?php echo form_submit('submit', 'Sign In','class="btn btn-primary"');?>
 </form>
 <a href="<?=site_url('auth/forgot_password');?>" title="Forgotten Password?">Forgotten Password?</a>
