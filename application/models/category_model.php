742c09cf1237f4bb3d4df0f9de8e46bc9485fe0a
peterpan <isaak@mogetutu.com>
2012-06-19 17:37:44 +0300

    Created category model

---
+++ application/models/category_model.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Category_model extends MY_Model
+{
+	public function __construct()
+	{
+	   parent::__construct();
+	   //Do your magic here
+	}
+}
