211478f4f8def3725f0469209ab11e0ec3536684
peterpan <isaak@mogetutu.com>
2012-06-27 02:26:55 +0300

    removed session_useragent check

--- application/config/testing/config.php
+++ application/config/testing/config.php
@@ -285,7 +285,7 @@ $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= TRUE;
 $config['sess_table_name']		= 'm_sessions';
 $config['sess_match_ip']		= FALSE;
-$config['sess_match_useragent']	= TRUE;
+$config['sess_match_useragent']	= FALSE;
 $config['sess_time_to_update']	= 300;
 
 /*
