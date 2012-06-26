8876bbe0f7120990272605725260d9625b3791b2
peterpan <isaak@mogetutu.com>
2012-06-27 02:58:47 +0300

    Finished Design of blog page

--- application/views/blog/index.php
+++ application/views/blog/index.php
@@ -1,14 +1,14 @@
 <div class="maincontent">
         <div class="col-2-2">
-            <div class="blog-post"><!-- blog post 1 --> 
+            <?php foreach ($posts as $post): ?>
+            <div class="blog-post"><!-- blog posts here --> 
                 <div class="post-image">
                     <img src="<?=base_url();?>media/site/images/blog-img1-1.jpg" alt="" class="imgborder2" />                                  
                 </div>
                 <div class="post-content2">
-                    <h5><a href="single-layout2.html">Nokia with First Windows Phone Handsets</a></h5>
+                    <h5><a href="<?=site_url('blog/post/');?>">Nokia with First Windows Phone Handsets</a></h5>
                     <div class="post-info2">
                         <span><img src="<?=base_url();?>media/site/images/blog-author.png" alt="" /></span> <span><a href="#">John Doe</a> &nbsp;&nbsp;|&nbsp;&nbsp; </span>
-                        <span><img src="<?=base_url();?>media/site/images/blog-comment.png" alt="" /></span> <span><a href="#">8 Comments</a> &nbsp;&nbsp;|&nbsp;&nbsp; </span>
                         <span><img src="<?=base_url();?>media/site/images/blog-date.png" alt="" /></span> <span>Okt 28 2011</span>
                     </div>
                     <p>Nokia yesterday unveiled a new crop of smartphones that could put the company back in the smartphone race with rivals iPhone and Android. It could also mark a turnaround for Microsoft's unpopular Windows Phone 7 software.</p>
@@ -20,85 +20,11 @@
                             <li><a href="#"><img src="<?=base_url();?>media/site/images/social-icons/top-social/social4.png" alt="" /></a></li>
                             <li><a href="#"><img src="<?=base_url();?>media/site/images/social-icons/top-social/social5.png" alt="" /></a></li>
                         </ul>
-                        <a href="single-layout2.html" class="button small gray"><span>read full article &raquo;</span></a>
+                        <a href="<?=site_url('blog/post/');?>" class="button small gray"><span>read full article &raquo;</span></a>
                     </div>                                
                 </div>                            
             </div>
-            
-            <div class="blog-post"><!-- blog post 2 --> 
-                <div class="post-image">
-                    <img src="<?=base_url();?>media/site/images/blog-img1-2.jpg" alt="" class="imgborder2" />                                  
-                </div>
-                <div class="post-content2">
-                    <h5><a href="single-layout2.html">Artificial Blood That Could be Used Transfusions</a></h5>
-                    <div class="post-info2">
-                        <span><img src="<?=base_url();?>media/site/images/blog-author.png" alt="" /></span> <span><a href="#">John Doe</a> &nbsp;&nbsp;|&nbsp;&nbsp; </span>
-                        <span><img src="<?=base_url();?>media/site/images/blog-comment.png" alt="" /></span> <span><a href="#">4 Comments</a> &nbsp;&nbsp;|&nbsp;&nbsp; </span>
-                        <span><img src="<?=base_url();?>media/site/images/blog-date.png" alt="" /></span> <span>Okt 15 2011</span>
-                    </div>
-                    <p>The scientists behind the research, which could provide industrial scale quantities of blood, believe it will transform transfusions by preventing hospital shortages, and save thousands of lives on battlefields and at the scene of car crashes.</p>
-                    <div class="social-button">
-                        <ul class="sharesocial-bloglist">
-                            <li><a href="#"><img src="<?=base_url();?>media/site/images/social-icons/top-social/social1.png" alt="" /></a></li>
-                            <li><a href="#"><img src="<?=base_url();?>media/site/images/social-icons/top-social/social2.png" alt="" /></a></li>
-                            <li><a href="#"><img src="<?=base_url();?>media/site/images/social-icons/top-social/social3.png" alt="" /></a></li>
-                            <li><a href="#"><img src="<?=base_url();?>media/site/images/social-icons/top-social/social4.png" alt="" /></a></li>
-                            <li><a href="#"><img src="<?=base_url();?>media/site/images/social-icons/top-social/social5.png" alt="" /></a></li>
-                        </ul>
-                        <a href="single-layout2.html" class="button small gray"><span>read full article &raquo;</span></a>
-                    </div>                                
-                </div>                            
-            </div>
-            
-            <div class="blog-post"><!-- blog post 3 --> 
-                <div class="post-image">
-                    <img src="<?=base_url();?>media/site/images/blog-img1-3.jpg" alt="" class="imgborder2" />                                  
-                </div>
-                <div class="post-content2">
-                    <h5><a href="single-layout2.html">Supercar A Bolt from Les Bleus</a></h5>
-                    <div class="post-info2">
-                        <span><img src="<?=base_url();?>media/site/images/blog-author.png" alt="" /></span> <span><a href="#">John Doe</a> &nbsp;&nbsp;|&nbsp;&nbsp; </span>
-                        <span><img src="<?=base_url();?>media/site/images/blog-comment.png" alt="" /></span> <span><a href="#">12 Comments</a> &nbsp;&nbsp;|&nbsp;&nbsp; </span>
-                        <span><img src="<?=base_url();?>media/site/images/blog-date.png" alt="" /></span> <span>Sept 24 2011</span>
-                    </div>
-                    <p>The French may have fallen just short of conquering the rugby world but they can still lay claim to the world's fastest production car, the Bugatti Veyron. And now a second French car maker is attempting to launch a supercar.</p>
-                    <div class="social-button">
-                        <ul class="sharesocial-bloglist">
-                            <li><a href="#"><img src="<?=base_url();?>media/site/images/social-icons/top-social/social1.png" alt="" /></a></li>
-                            <li><a href="#"><img src="<?=base_url();?>media/site/images/social-icons/top-social/social2.png" alt="" /></a></li>
-                            <li><a href="#"><img src="<?=base_url();?>media/site/images/social-icons/top-social/social3.png" alt="" /></a></li>
-                            <li><a href="#"><img src="<?=base_url();?>media/site/images/social-icons/top-social/social4.png" alt="" /></a></li>
-                            <li><a href="#"><img src="<?=base_url();?>media/site/images/social-icons/top-social/social5.png" alt="" /></a></li>
-                        </ul>
-                        <a href="single-layout2.html" class="button small gray"><span>read full article &raquo;</span></a>
-                    </div>                                
-                </div>                            
-            </div> 
-
-            <div class="blog-post"><!-- blog post 4 --> 
-                <div class="post-image">
-                    <img src="<?=base_url();?>media/site/images/blog-img1-3.jpg" alt="" class="imgborder2" />                                  
-                </div>
-                <div class="post-content2">
-                    <h5><a href="single-layout2.html">HotCar A Bolt from The Blues</a></h5>
-                    <div class="post-info2">
-                        <span><img src="<?=base_url();?>media/site/images/blog-author.png" alt="" /></span> <span><a href="#">John Doe</a> &nbsp;&nbsp;|&nbsp;&nbsp; </span>
-                        <span><img src="<?=base_url();?>media/site/images/blog-comment.png" alt="" /></span> <span><a href="#">12 Comments</a> &nbsp;&nbsp;|&nbsp;&nbsp; </span>
-                        <span><img src="<?=base_url();?>media/site/images/blog-date.png" alt="" /></span> <span>Sept 24 2011</span>
-                    </div>
-                    <p>The French may have fallen just short of conquering the rugby world but they can still lay claim to the world's fastest production car, the Bugatti Veyron. And now a second French car maker is attempting to launch a supercar.</p>
-                    <div class="social-button">
-                        <ul class="sharesocial-bloglist">
-                            <li><a href="#"><img src="<?=base_url();?>media/site/images/social-icons/top-social/social1.png" alt="" /></a></li>
-                            <li><a href="#"><img src="<?=base_url();?>media/site/images/social-icons/top-social/social2.png" alt="" /></a></li>
-                            <li><a href="#"><img src="<?=base_url();?>media/site/images/social-icons/top-social/social3.png" alt="" /></a></li>
-                            <li><a href="#"><img src="<?=base_url();?>media/site/images/social-icons/top-social/social4.png" alt="" /></a></li>
-                            <li><a href="#"><img src="<?=base_url();?>media/site/images/social-icons/top-social/social5.png" alt="" /></a></li>
-                        </ul>
-                        <a href="single-layout2.html" class="button small gray"><span>read full article &raquo;</span></a>
-                    </div>                                
-                </div>                            
-            </div>                 
+            <?php endforeach ?>                
             
             <!-- begin of pagination -->
             <div class="blog-pagination">
