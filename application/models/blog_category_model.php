c4585bf34125da79ddb55cc68c3e6b4b80024467
peterpan <isaak@mogetutu.com>
2012-06-27 02:57:20 +0300

    create blog and blog_category models for Blog

---
+++ application/models/blog_category_model.php
@@ -0,0 +1,23 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Blog_category_model extends MY_Model {
+
+	public $before_create = array('date_modified');
+	public $before_update = array('date_modified');
+
+	public function __construct()
+	{
+		parent::__construct();
+		
+	}
+
+	public function date_modified($category)
+	{
+		$category->date_modified = date('Y-m-d H:i:s');
+		return $category;
+	}
+
+}
+
+/* End of file blog_category_model.php */
+/* Location: ./application/models/blog_category_model.php */
