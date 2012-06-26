fe5d17dc5b08ecaf636ed3ea8668802fca607103
peterpan <isaak@mogetutu.com>
2012-06-27 02:59:49 +0300

    removed price feed from post model

--- application/models/post_model.php
+++ application/models/post_model.php
@@ -45,6 +45,12 @@ class Post_model extends MY_Model
 		return $this;
 	}
 
+	public function live()
+	{
+		$this->db->where('post_status','LIVE');
+		return $this;
+	}
+
 	public function with_order_details()
 	{
 		$this->db->join('order_details','posts.id = order_details.post_id','left');
@@ -102,22 +108,4 @@ class Post_model extends MY_Model
 		$this->db->where('id', $ids);
 	}
 
-	public function price_feed($crop)
-	{	
-		$Q = 'SELECT product_name,crop_weight,crop_unit,crop_date,DATE_FORMAT(crop_date,"%a") as wk_date,
-			MAX(if(location_id = 43,crop_price, NULL)) AS "NBI",
-			MAX(if(location_id = 44,crop_price, NULL)) AS "MSA",
-			MAX(if(location_id = 45,crop_price, NULL)) AS "KSM",
-			MAX(if(location_id = 55,crop_price, NULL)) AS "ELD",
-			MAX(if(location_id = 56,crop_price, NULL)) AS "KTL"
-			FROM prices
-			JOIN products on products.id = prices.product_id
-			WHERE crop_date < (SELECT max(crop_date) FROM prices)
-			AND crop_date > ((SELECT max(crop_date) FROM prices) - INTERVAL 5 DAY)
-			AND prices.product_id = "'.$crop.'"
-			AND prices.status ="live"
-			GROUP BY prices.product_id,wk_date
-			ORDER BY crop_date DESC';
-		return $this->db->query($Q)->result();
-	}
 }
