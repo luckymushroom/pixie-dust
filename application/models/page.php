475a242c0e41ef87c9d97fba1e755b776a6dbffe
mogetutu <mogetutu@HackBook Pro.(none)>
2012-10-19 12:32:21 +0300

    Version 1.1
    Updated with aggregators and farmers panels

--- application/models/page.php
+++
@@ -1,8 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class Page extends ActiveRecord\Model {
-
-}
-
-/* End of file Press.php */
-/* Location: ./application/models/Press.php */
--- application/views/farm/index.php
+++
@@ -1,5 +0,0 @@
-<ul class="nav nav-pills">
-	<li><a href="<?=site_url('farm/details'); ?>">Shamba Details</a></li>
-	<li class="active"><a href="<?=site_url('farm/crops_grown'); ?>">Crops Grown</a></li>
-	<li><a href="<?=site_url('farm/harvest'); ?>">Harvests</a></li>
-</ul>
