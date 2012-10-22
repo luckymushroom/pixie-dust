f02b86e618f6ee7401bfe2ea2bd687151e3f02a2
luckymushroom <isaak@mogetutu.com>
2012-10-22 14:20:47 +0300

    Added aggregator products page

---
+++ application/controllers/aggregator/products.php
@@ -0,0 +1,17 @@
+<?php
+if (! defined('BASEPATH')) exit('No direct script access');
+
+class Products extends MY_Controller {
+
+    //php 5 constructor
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function index()
+    {
+
+    }
+
+}
